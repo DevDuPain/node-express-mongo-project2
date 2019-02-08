6477a960ac7f3fe4cd890a3f8de1b3d3def2bdfc
DevDuPain <MarlonsAddress@gmail.com>
2019-02-07 20:33:22 -0500

    dynamic export dev/prod keys

--- config/keys.js
+++ config/keys.js
@@ -1,14 +1,18 @@
-if(process.env.NODE_ENV === 'production'){
-  module.exports = {
+import secret from './secret';
+let keys = null;
+
+ if(process.env.NODE_ENV === 'production'){
+   keys = {
     mongoURI:PROCESS.ENV.MONGO_URI,
     googleClientID:PROCESS.ENV.GOOGLE_CLIENT_ID,
     googleClientSecret:PROCESS.ENV.GOOGLE_CLIENT_SECRET,
   }
 }else{
-  import secret from '/secret'
-  module.exports = {
+  keys = {
     mongoURI:secret.MONGO_URI,
     googleClientID:secret.GOOGLE_CLIENT_ID,
     googleClientSecret:secret.GOOGLE_CLIENT_SECRET
   }
 };
+
+export default keys;
