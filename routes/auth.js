1d5b8a6433089987c804761e9ac8eb8406ffec2a
DevDuPain <MarlonsAddress@gmail.com>
2019-02-09 14:27:33 -0500

    auth request

--- routes/auth.js
+++ routes/auth.js
@@ -4,4 +4,12 @@ import passport from 'passport'
 
 router.get('/google', passport.authenticate('google', {scope: ['profile', 'email']}));
 
+router.get('/google',
+  passport.authenticate('google', { scope: ['profile'] }));
+
+router.get('/google/callback', 
+  passport.authenticate('google', { failureRedirect: '/' }), (req, res) => {
+    res.redirect('/dashboard');
+  });
+
 export default router;
