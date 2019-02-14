902f4a658e985dd4142ec45b2e765b77650c21d8
DevDuPain <MarlonsAddress@gmail.com>
2019-02-13 23:55:36 -0500

    user session

--- config/passport.js
+++ config/passport.js
@@ -39,5 +39,13 @@ export default (passport) => {
         }
       })
     })
-  )
+  );
+
+  passport.serializeUser((user, done) => {
+    done(null, user.id);
+  });
+
+  passport.deserializeUser((id, done) => {
+    User.findById(id).then(user => done(null, user));
+  });
 }
